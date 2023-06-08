f2ca9a061468a27384b93e202d1aadad6b2176eb
pipiipin <74450504+pipiipin@users.noreply.github.com>
2023-06-08 17:12:44 +0700

    Update All

--- not_sanook/src/components/footer/Footer.jsx
+++ not_sanook/src/components/footer/Footer.jsx
@@ -1,77 +1,94 @@
 "use client";
 
-import React from 'react';
-import styles from './footer.module.css';
+import React from "react";
+import styles from "./footer.module.css";
 import Image from "next/image";
+import Sanook from "../../../public/sanook.png";
+import FB from "../../../public/facebook.png";
+import TW from "../../../public/twitter.png";
+import IG from "../../../public/instagram.png";
+import YT from "../../../public/youtube.png";
+import IN from "../../../public/linkedin.png";
+import LI from "../../../public/line.png";
 
 const Footer = () => {
-
   return (
     <div className={styles.container}>
-      <div> 
-        <h2>สงวนลิขสิทธิ์ © 2566 บริษัท เทนเซ็นต์ (ประเทศไทย) จำกัด</h2>
+      <div className={styles.boxLeft}>
+        <Image src={Sanook} width={120} height={17} alt="Sanook icon" />
+        <h4>สงวนลิขสิทธิ์ © 2566 บริษัท เทนเซ็นต์ (ประเทศไทย) จำกัด</h4>
+        <div className={styles.suggest}>
+          <p
+            onClick={() => {
+              console.log("About us");
+            }}
+          >
+            เกี่ยวกับเรา
+          </p>
+          <div className={styles.divider} />
+          <p
+            onClick={() => {
+              console.log("Privacy Policy");
+            }}
+          >
+            นโยบายความเป็นส่วนตัว
+          </p>
+          <div className={styles.divider} />
+          <p
+            onClick={() => {
+              console.log("Cookie Policy");
+            }}
+          >
+            นโยบายคุกกี้
+          </p>
+          <div className={styles.divider} />
+          <p
+            onClick={() => {
+              console.log("Inappropriate content");
+            }}
+          >
+            ร้องเรียนเนื้อหาไม่เหมาะสม
+          </p>
+        </div>
       </div>
-      <div className={styles.social}>
-        <Image src="/sanook.png" width={195} height={35} alt="Sanook"/>
-        <Image src="/1.png" width={35} height={35} className={styles.icon} alt="Facebook"/>
-        <Image src="/2.png" width={35} height={35} className={styles.icon} alt="Twitter"/>
-        <Image src="/3.png" width={35} height={35} className={styles.icon} alt="Instagram"/>
-        <Image src="/4.png" width={35} height={35} className={styles.icon} alt="Youtube"/>
-        <Image src="/5.png" width={35} height={35} className={styles.icon} alt="LinkedIn"/>
-        <Image src="/6.png" width={35} height={35} className={styles.icon} alt="Line"/>
+      <div className={styles.boxRight}>
+        <p
+          onClick={() => {
+            console.log("Give feedback, report problems");
+          }}
+        >
+          แนะนำติชม แจ้งปัญหาการใช้งาน
+        </p>
+        <p
+          onClick={() => {
+            console.log("Work with uss");
+          }}
+        >
+          ร่วมงานกับเรา
+        </p>
+        <div className={styles.contact}>
+          <div className={styles.social}>
+            <Image src={FB} className={styles.icon} alt="Facebook" />
+            <Image src={TW} className={styles.icon} alt="Twitter" />
+            <Image src={IG} className={styles.icon} alt="Instagram" />
+            <Image src={YT} className={styles.icon} alt="Youtube" />
+            <Image src={IN} className={styles.icon} alt="LinkedIn" />
+            <Image src={LI} className={styles.icon} alt="Line" />
+          </div>
+          <div class="searchBox">
+            <form noValidate action="" role="search">
+              <input
+              className={styles.input}
+              type="text"
+                placeholder="Email address"
+              />
+              <button className={styles.button} type="submit">รับข่าวสาร</button>
+            </form>
+          </div>
+        </div>
       </div>
-      <div className={styles.social}>
-      <button
-       className={styles.login}
-       onClick={() => {
-        console.log("Log in");
-       }}
-       >
-        แนะนำติชม แจ้งปัญหาการใช้งาน
-      </button>
-      <button
-       className={styles.login}
-       onClick={() => {
-        console.log("Log in");
-       }}
-       >
-        ร่วมงานกับเรา
-      </button>
-      <button
-       className={styles.login}
-       onClick={() => {
-        console.log("Log in");
-       }}
-       >
-        เกี่ยวกับเรา
-       </button>
-       <button
-       className={styles.login}
-       onClick={() => {
-        console.log("Log in");
-       }}
-       >
-        นโยบายความเป็นส่วนตัว
-       </button>
-       <button
-       className={styles.login}
-       onClick={() => {
-        console.log("Log in");
-       }}
-       >
-        นโยบายคุกกี้
-       </button>
-       <button
-       className={styles.login}
-       onClick={() => {
-        console.log("Log in");
-       }}
-       >
-        ร้องเรียนเนื้อหาไม่เหมาะสม
-       </button>
-       </div>
     </div>
   );
 };
 
-export default Footer
+export default Footer;
--- not_sanook/src/components/navbar/Navbar.jsx
+++ not_sanook/src/components/navbar/Navbar.jsx
@@ -1,90 +1,146 @@
 "use client";
 
-import Link from 'next/link';
-import React from 'react';
+import Link from "next/link";
+import React from "react";
 import styles from "./navbar.module.css";
 import Image from "next/image";
+import Sanook from "../../../public/sanook.png";
+import Search from "../../../public/search.png";
 //import { signIn, useSession } from "next-auth/react";
 
 const links = [
-    {
-      id: 1,
-      title: "ข่าว",
-      url: "/",
-    },
-    {
-      id: 2,
-      title: "ดูดวง-ตรวจหวย",
-      url: "/portfolio",
-    },
-    {
-      id: 3,
-      title: "ไลฟ์สไตล์",
-      url: "/blog",
-    },
-    {
-      id: 4,
-      title: "บันเทิงครบรส",
-      url: "/about",
-    },
-    {
-      id: 5,
-      title: "ความรู้รอบตัว",
-      url: "/contact",
-    },
-    {
-      id: 6,
-      title: "Dashboard",
-      url: "/dashboard",
-    },
-  ];
+  {
+    id: 1,
+    title: "ข่าว",
+    url: "/",
+  },
+  {
+    id: 2,
+    title: "ดูดวง-ตรวจหวย",
+    url: "/portfolio",
+  },
+  {
+    id: 3,
+    title: "ไลฟ์สไตล์",
+    url: "/blog",
+  },
+  {
+    id: 4,
+    title: "บันเทิงครบรส",
+    url: "/about",
+  },
+  {
+    id: 5,
+    title: "ความรู้รอบตัว",
+    url: "/contact",
+  },
+  {
+    id: 6,
+    title: "Dashboard",
+    url: "/dashboard",
+  },
+];
 
 const Navbar = () => {
   //const session = useSession();
 
   return (
     <div className={styles.container}>
-        <Link href="/" className={styles.logo}>
-        <Image src="/sanook.png" width={195} height={45} alt="Sanook"/>
-        </Link>
-        <h4 className={styles.most}>2 June 2023</h4>
-        <div className={styles.link}>
-        {links.map((link) => (
-          <Link key={link.id} href={link.url} className={styles.link}>
-            {link.title}
+      <div className={styles.nav1}>
+        <div className={styles.navLeft}>
+          <Link href="/">
+            <Image src={Sanook} className={styles.logo} alt="Sanook icon" />
           </Link>
-        ))}
-        <div className={styles.social}>
-        <Image src="/Lotto.png" width={35} height={35} className={styles.icon} alt="ตรวจหวย"/>
-        <h5 className={styles.most}>ตรวจหวย</h5>
-        <Image src="/Joox.png" width={35} height={35} className={styles.icon} alt="Joox"/>
-        <h5 className={styles.most}>Joox</h5>
-        <Image src="/TV.png" width={35} height={35} className={styles.icon} alt="TV"/>
-        <h5 className={styles.most}>WeTV</h5>
-        <Image src="/Comic.png" width={35} height={35} className={styles.icon} alt="Comics"/>
-        <h5 className={styles.most}>WeComics</h5>
-        <Image src="/Pubg.png" width={35} height={35} className={styles.icon} alt="PubG"/>
-        <h5 className={styles.most}>PUBG</h5>
-      </div>
-       <button
-       className={styles.login}
-       onClick={() => {
-        console.log("Log in");
-       }}
-       >
-        Login
-       </button>
-       <button
-       className={styles.login}
-       onClick={() => {
-        console.log("Register");
-       }}
-       >
-        Register
-       </button>
+          <p className={styles.most}>2 June 2023</p>
         </div>
+        <div className={styles.navMid}>
+          <input className={styles.input} type="text" placeholder="Search" />
+          <Image
+            className={styles.inputIcon}
+            src={Search}
+            width={20}
+            height={20}
+            alt="Search icon"
+          />
+        </div>
+        <div className={styles.navRight}>
+          <h4
+            onClick={() => {
+              console.log("Log in");
+            }}
+          >
+            Login
+          </h4>
+          <div className={styles.divider} />
+          <h4
+            onClick={() => {
+              console.log("Register");
+            }}
+          >
+            Register
+          </h4>
+        </div>
+      </div>
+      <div className={styles.nav2}>
+        <div className={styles.navLeft}>
+          {links.map((link) => (
+            <Link key={link.id} href={link.url} className={styles.category}>
+              <h4>{link.title}</h4>
+            </Link>
+          ))}
+        </div>
+        <div className={styles.navRight}>
+          <div className={styles.feature}>
+            <div className={styles.app}>
+              <Image
+                src="/Lotto.png"
+                width={30}
+                height={30}
+                alt="ตรวจหวย"
+              />
+              <h5 className={styles.most}>ตรวจหวย</h5>
+            </div>
+            <div className={styles.app}>
+              <Image
+                src="/Joox.png"
+                width={30}
+                height={30}
+                alt="Joox"
+              />
+              <h5 className={styles.most}>Joox</h5>
+            </div>
+            <div className={styles.app}>
+              <Image
+                src="/TV.png"
+                width={30}
+                height={30}
+                alt="TV"
+              />
+              <h5 className={styles.most}>WeTV</h5>
+            </div>
+            <div className={styles.app}>
+              <Image
+                src="/Comic.png"
+                width={30}
+                height={30}
+                alt="Comics"
+              />
+              <h5 className={styles.most}>WeComics</h5>
+            </div>
+            <div className={styles.app}>
+              <Image
+                src="/Pubg.png"
+                width={30}
+                height={30}
+                alt="PubG"
+              />
+              <h5 className={styles.most}>PUBG</h5>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
 
-export default Navbar
+export default Navbar;
